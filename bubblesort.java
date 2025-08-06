9f8aace0740dbbeb12ba714b2e6915f18a1908d3
Prem Bijarane <165685254+prembijarane5112@users.noreply.github.com>
2025-08-06 17:36:23 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,10 @@
+public class bubblesort{
+
+  public static void sort(int array[]){//create a method
+    
+    
+    public static void main(String  args[])
+      {
+      int array[]=4,3,7,5,1,2};//random array not sorted
+    
+    
