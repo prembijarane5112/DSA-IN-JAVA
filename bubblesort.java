372bd8228c7410d7bb54fda8c51e92d077b152d9
Prem Bijarane <165685254+prembijarane5112@users.noreply.github.com>
2025-08-06 17:38:12 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,6 +1,11 @@
 public class bubblesort{
 
   public static void sort(int array[]){//create a method
+    for(int turn=0;turn<array.length-1;i++)
+      {
+        for(int j=0;j<array.length-1-turn;j++)
+          {
+            
     
     
     public static void main(String  args[])
