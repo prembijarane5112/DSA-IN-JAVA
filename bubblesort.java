dff0fb98512c89067289f7fd927fd55336d00439
Prem Bijarane <165685254+prembijarane5112@users.noreply.github.com>
2025-08-06 17:43:38 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -5,11 +5,30 @@ public class bubblesort{
       {
         for(int j=0;j<array.length-1-turn;j++)
           {
+            if(array[j] > array[j+1]
+               {
+              int temp=array[j];
+              array[j]=array[j+1];
+              array[j+1]=temp;
+            }
+          }
+      }
+    public static printarr(int array[])
+      {
+for(int i=0;i<array.length;i++)
+  {
+    System.out.print(array[i]+" ");
+  }
+      System.out.println();
+    }
+              
             
     
     
     public static void main(String  args[])
       {
       int array[]=4,3,7,5,1,2};//random array not sorted
-    
-    
+    sort(array);//call a sort function
+    printarr(array);//call printarr function
+  }
+}
